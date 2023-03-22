e583593ca5e2f3b40101572fa211ca340212c7a9
Phan Quoc Tuan <21521637@gm.uit.edu.vn>
2023-03-22 12:02:26 +0700

    front end

---
+++ frontend/src/api/posts.tsx
@@ -0,0 +1,81 @@
+import axios from 'axios'
+
+export interface Empolyee{
+    id:string ,
+    EmpId:string ,
+    FullName:string ,
+    DateOfBirth:Date,
+    Address:string,
+    Birthplace: string,
+    PhoneNumber:string,
+    Email:String,
+    CreatedDate:Date,
+    UpdatedDate: Date,
+    avatarURL:string,
+}
+
+
+export const useApi=()=>{
+    const api=axios.create({
+        baseURL:'http://101.99.31.151:3002/employees'
+    })
+    
+    const getEmp= async():Promise<any>=>{
+        const result= await api.get('/');
+        return result;
+    }
+
+    const delEmp= async(Id:string):Promise<any>=>{
+        const resul=await api.delete(`/${Id}`)
+        // .then(res=>{
+        //     console.log(res);
+            
+        //     return res;            
+        //  }
+        // )
+        // .catch(error=>{
+        //     console.log(error);          
+        //     return error;
+        // })
+
+        
+        return resul
+    }
+
+    const addEmp= async(data:any):Promise<any>=>{
+        
+        const resul=await api.post('/',data)
+        .then(res=>{
+    
+            return res;            
+         }
+        )
+        .catch(error=>{
+            console.log(error);          
+            return error;
+        })     
+        return resul
+    }
+
+    const patchEmp= async(id:string,data:any):Promise<any>=>{
+        const result= await api.patch(`/${id}`,data)
+        .then(res=>{
+    
+            return res;            
+         }
+        )
+        .catch(error=>{
+            console.log(error);          
+            return error;
+        })     
+        return result;
+    }
+
+    return {
+        getEmp,
+        delEmp,
+        addEmp,
+        patchEmp
+    }
+}
+export default useApi;
